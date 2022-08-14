63c67c2446193f9eb48e4d6dc9d76ece3948f4d1
takagen99 <53655239+takagen99@users.noreply.github.com>
2022-08-14 20:48:20 +0800

    Add Wallpapers

--- wallpapers/index.php
+++ wallpapers/index.php
@@ -12,7 +12,9 @@ $myLinks = array("https://takagen99.github.io/wallpapers/01.png",
     "https://takagen99.github.io/wallpapers/07.png",
     "https://takagen99.github.io/wallpapers/08.png",
     "https://takagen99.github.io/wallpapers/09.png",
-	"https://takagen99.github.io/wallpapers/10.png");
+    "https://takagen99.github.io/wallpapers/10.png",
+    "https://takagen99.github.io/wallpapers/11.jpg",
+	"https://takagen99.github.io/wallpapers/12.jpg");
 
 $randomRedirection = $myLinks[array_rand($myLinks)];
 header("Location: $randomRedirection");
