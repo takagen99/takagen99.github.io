cc3a528bc8bd91beec678d75536851d5899c2c86
takagen99 <53655239+takagen99@users.noreply.github.com>
2022-08-20 19:59:15 +0800

    Active Forks

--- active-forks/js/main.js
+++ active-forks/js/main.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', () => {
   initDT(); // Initialize the DatatTable and window.columnNames variables
-  addDarkmodeWidget();
 
   const repo = getRepoFromUrl();
 
@@ -15,12 +14,8 @@ document.getElementById('form').addEventListener('submit', e => {
   fetchData();
 });
 
-function addDarkmodeWidget() {
-  new Darkmode( { label: '🌓' } ).showWidget();
-}
-
 function fetchData() {
-  const repo = document.getElementById('q').value.replaceAll(' ','');
+  const repo = document.getElementById('q').value;
   const re = /[-_\w]+\/[-_.\w]+/;
 
   const urlRepo = getRepoFromUrl();
@@ -39,21 +34,28 @@ function fetchData() {
   }
 }
 
-function updateDT(data) {
+function updateDT(data, table) {
   // Remove any alerts, if any:
   if ($('.alert')) $('.alert').remove();
 
   // Format dataset and redraw DataTable. Use second index for key name
   const forks = [];
+  if (!Array.isArray(data)) {
+    data = [data]
+  }
   for (let fork of data) {
-    fork.repoLink = `<a href="https://github.com/${fork.full_name}">Link</a>`;
-    fork.ownerName = `<img src="${fork.owner.avatar_url || 'https://avatars.githubusercontent.com/u/0?v=4'}&s=48" width="24" height="24" class="mr-2 rounded-circle" />${fork.owner ? fork.owner.login : '<strike><em>Unknown</em></strike>'}`;
+    fork.repository = `<a href="https://github.com/${fork.full_name}">${fork.full_name}</a>`;
+    fork.ownerName = fork.owner.login;
+    let push = moment(fork.pushed_at);
+    let create = moment(fork.created_at);
+    let update = moment(fork.updated_at);
+    fork.changed = (push.diff(create, 'minutes') > 0 || update.diff(create, 'minutes') > 0) ? '+' : '-';
     forks.push(fork);
   }
   const dataSet = forks.map(fork =>
     window.columnNamesMap.map(colNM => fork[colNM[1]])
   );
-  window.forkTable
+  table
     .clear()
     .rows.add(dataSet)
     .draw();
@@ -62,16 +64,17 @@ function updateDT(data) {
 function initDT() {
   // Create ordered Object with column name and mapped display name
   window.columnNamesMap = [
-    // [ 'Repository', 'full_name' ],
-    ['Link', 'repoLink'], // custom key
-    ['Owner', 'ownerName'], // custom key
-    ['Name', 'name'],
+    ['Repository', 'repository'], // custom key
     ['Branch', 'default_branch'],
     ['Stars', 'stargazers_count'],
     ['Forks', 'forks'],
-    ['Open Issues', 'open_issues_count'],
-    ['Size', 'size'],
-    ['Last Push', 'pushed_at'],
+    ['Issues', 'open_issues_count'],
+    ['Watched', 'watchers'],
+    // ['Size', 'size'],
+    ['Pushed', 'pushed_at'],
+    ['Updated', 'updated_at'],
+    ['Created', 'created_at'],
+    ['Edited', 'changed'],
   ];
 
   // Sort by stars:
@@ -80,32 +83,35 @@ function initDT() {
     .map(pair => pair[0])
     .indexOf(sortColName);
 
+  let colMapper = window.columnNamesMap.map(colNM => {
+    return {
+      title: colNM[0],
+      render:
+        colNM[1] === 'pushed_at' || colNM[1] === 'created_at' || colNM[1] === 'updated_at'
+          ? (data, type, _row) => {
+              if (type === 'display') {
+                return moment(data).format('YYYY-MM-DD HH:mm');
+              }
+              return data;
+            }
+          : null,
+    };
+  });
+
   // Use first index for readable column name
   // we use moment's fromNow() if we are rendering for `pushed_at`; better solution welcome
   window.forkTable = $('#forkTable').DataTable({
-    columns: window.columnNamesMap.map(colNM => {
-      return {
-        title: colNM[0],
-        render:
-          colNM[1] === 'pushed_at'
-            ? (data, type, _row) => {
-                if (type === 'display') {
-                  return moment(data).fromNow();
-                }
-                return data;
-              }
-            : null,
-      };
-    }),
+    columns: colMapper,
     order: [[sortColumnIdx, 'desc']],
-    // paging: false,
-    searchBuilder:{
-      // all options at default
-    }
   });
-  let table = window.forkTable;
-  new $.fn.dataTable.SearchBuilder(table, {});
-  table.searchBuilder.container().prependTo(table.table().container());
+
+  window.originTable = $('#originTable').DataTable({
+    paging: false,
+    searching: false,
+    ordering: false,
+    info: false,
+    columns: colMapper,
+  });
 }
 
 function fetchAndShow(repo) {
@@ -114,15 +120,27 @@ function fetchAndShow(repo) {
   repo = repo.replace(/\.git$/, '');
 
   fetch(
-    `https://api.github.com/repos/${repo}/forks?sort=stargazers&per_page=100&page=1`
-	//`https://api.github.com/repos/${repo}/forks?sort=stargazers&per_page=100&page=2`
+    `https://api.github.com/repos/${repo}/forks?sort=stargazers&per_page=100`
   )
     .then(response => {
       if (!response.ok) throw Error(response.statusText);
       return response.json();
     })
     .then(data => {
-      updateDT(data);
+      console.log(data);
+      updateDT(data, window.forkTable);
+
+      return fetch(
+        `https://api.github.com/repos/${repo}`
+      )
+    })
+    .then(response => {
+      if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      updateDT(data, window.originTable);
     })
     .catch(error => {
       const msg =
